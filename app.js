c08a286a5eaba1cbab5e23f71a10a0d9bd04ff8e
275313730 <a275313730@qq.com>
2020-04-11 22:49:41 +0800

    2020-04-11 update

--- app.js
+++ app.js
@@ -1,22 +1,15 @@
-const router = {
-    alive: true,
-    routes: [{
-        path: 'game',
-        component: game
-    }, {
-        path: 'score',
-        component: score
-    }]
-}
-
 Link.$store = { X: 0, O: 0 }
 
 new Link({
     el: 'app',
-    data() {
-        return {
-            str: 'Tic-Tac-Toe'
-        }
-    },
-    router
+    router: {
+        alive: true,
+        routes: [{
+            path: 'game',
+            component: game
+        }, {
+            path: 'score',
+            component: score
+        }]
+    }
 })
