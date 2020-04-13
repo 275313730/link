8988f5bb55ad03c2fa73054cedf7926b9d136a4a
275313730 <a275313730@qq.com>
2020-04-13 14:43:04 +0800

    2020-04-13 update

--- Pack.js
+++ Pack.js
@@ -1,51 +1,39 @@
-// use reg /^\s*(?=\r?$)\n/ to delete the space line
+// change the urls according to your files
+// put this script in the index.html for the first script
 const urls = {
-    html: 'index',
     entry: 'App',
     modules: ['Link', 'link-router'],
     views: ['Game', 'Score'],
     components: ['Test', 'Cpn']
 }
 
-parseUrls()
+const pack = document.getElementsByTagName('script')[0]
 
-function parseUrls() {
-    let html = getFileText(`${urls.html}.html`),
-        templates = '',
-        scripts = '',
-        styles = ''
+urls.modules.forEach(name => {
+    document.write(`<script type="text/javascript" src="modules/${name}.js"></script>`);
+})
 
-    urls['modules'].forEach(name => {
-        scripts += `<script src="modules/${name}.js" ></script>`
+urls.components.forEach(url => {
+    parseCpn(url)
+});
+
+urls.views.forEach(url => {
+    parseCpn(url)
+});
+
+document.write(`<script src="${urls.entry}.js" ></script>`)
+
+pack.parentNode.removeChild(pack)
+
+function parseCpn(url) {
+    const tags = ['template', 'script', 'style'],
+        text = getFileText(`views/${url}.Link`)
+
+    tags.forEach(tag => {
+        const start = text.indexOf(`<${tag}`),
+            end = text.indexOf(`/${tag}>`) + `/${tag}>`.length
+        document.write(text.slice(start, end));
     })
-
-    urls['components'].forEach(url => {
-        parseCpn(url)
-    });
-
-    urls['views'].forEach(url => {
-        parseCpn(url)
-    });
-
-    scripts += `<script src="${urls.entry}.js" ></script>`
-
-    html = html.replace(/\<script.*\/script>/, templates + scripts + styles)
-    console.log(html)
-
-    function parseCpn(url) {
-        let text = getFileText(`views/${url}.html`),
-            start = text.indexOf('<template'),
-            end = text.indexOf('/template>') + '/template>'.length
-        templates += text.slice(start, end) + '\n'
-
-        start = text.indexOf('<script')
-        end = text.indexOf('/script>') + '/script>'.length
-        scripts += text.slice(start, end) + '\n'
-
-        start = text.indexOf('<style')
-        end = text.indexOf('/style>') + '/style>'.length
-        styles += text.slice(start, end) + '\n'
-    }
 }
 
 function getFileText(url) {
